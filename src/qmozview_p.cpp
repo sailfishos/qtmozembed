633b274cfd7fdc6cd935f06fddcc1f1016493919
Pavel Tumakaev <p.tumakaev@omprussia.ru>
2020-11-11 19:13:47 +0300

    [qtmozembed] Introduce virtualKeyboardHeight property to forward VKB height change event to EmbedLiteView. Contributes to JB#51962

--- src/qmozview_p.cpp
+++ src/qmozview_p.cpp
@@ -1019,6 +1019,13 @@ void QMozViewPrivate::setThrottlePainting(bool aThrottle)
     }
 }
 
+void QMozViewPrivate::SetVirtualKeyboardHeight(int aHeight)
+{
+    if (mViewInitialized) {
+        mView->SetVirtualKeyboardHeight(aHeight);
+    }
+}
+
 void QMozViewPrivate::IMENotification(int aIstate, bool aOpen, int aCause, int aFocusChange,
                                       const char16_t *inputType, const char16_t *inputMode)
 {
